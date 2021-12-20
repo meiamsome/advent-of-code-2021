f751ecbe2fca83a115c8acb5620558c77cb86cdd
Oliver Wright <meiamsome@meiamso.me>
2021-12-20 21:05:30 +0000

    D3P1: Computation

--- day03/part1/src/main.rs
+++ day03/part1/src/main.rs
@@ -18,14 +18,42 @@ fn parse_input<'a>(input: &'a str) -> (usize, impl Iterator<Item = u16> + 'a) {
     (bits, line_iter.map(|x| u16::from_str_radix(x, 2).unwrap()))
 }
 
+fn calculate_rates(bits: usize, diagnostic_input: &mut dyn Iterator<Item = u16>) -> (u16, u16) {
+    let mut counters = vec![0usize; bits];
+    let mut total = 0;
+    for value in diagnostic_input {
+        total += 1;
+        for i in 0..bits {
+            if value & (1 << i) != 0 {
+                counters[i] += 1;
+            }
+        }
+    }
+
+    let mut epsilon = 0;
+    let mut gamma = 0;
+    for i in 0..bits {
+        if counters[i] > total / 2 {
+            gamma |= 1 << i;
+        } else {
+            epsilon |= 1 << i;
+        }
+    }
+
+    (gamma, epsilon)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut contents = String::new();
     {
-        let mut file = File::open("./day01/part1/input.txt")?;
+        let mut file = File::open("./day03/part1/input.txt")?;
         file.read_to_string(&mut contents)?;
     }
 
-    let _input = parse_input(&contents);
+    let (bits, mut diagnostic_input) = parse_input(&contents);
+    let (gamma, epsilon) = calculate_rates(bits, &mut diagnostic_input);
+
+    println!("Power consumption {}", gamma as u32 * epsilon as u32);
 
     Ok(())
 }
@@ -58,5 +86,15 @@ mod test {
             iter.collect::<Vec<_>>(),
             vec![4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10],
         );
+    }
+
+    #[test]
+    fn example_calculate() {
+        let input = vec![4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10];
+
+        let (gamma, epsilon) = calculate_rates(5, &mut input.into_iter());
+
+        assert_eq!(gamma, 22);
+        assert_eq!(epsilon, 9);
     }
 }
