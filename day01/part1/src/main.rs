0239d12b231c85014e7747cf769ba2d8f37df625
Oliver Wright <meiamsome@meiamso.me>
2021-12-20 19:29:04 +0000

    Switch to windows

--- day01/part1/src/main.rs
+++ day01/part1/src/main.rs
@@ -1,26 +1,24 @@
 use std::{fs::File, io::Read};
 
-fn get_depth_increases(iter: &mut dyn Iterator<Item = u16>) -> usize {
-    let mut last_value = iter.next().unwrap();
-    let mut increases = 0;
-    for value in iter {
-        if value > last_value {
-            increases += 1;
-        }
-        last_value = value;
-    }
-    increases
+fn get_depth_increases(list: &Vec<u16>) -> usize {
+    list.windows(2)
+        .map(|res| if res[0] < res[1] { 1 } else { 0 })
+        .sum()
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut contents = String::new();
     {
         let mut file = File::open("./day01/part1/input.txt")?;
         file.read_to_string(&mut contents)?;
     }
 
-    let depth_increases =
-        get_depth_increases(&mut contents.split_whitespace().map(|v| v.parse().unwrap()));
+    let values = contents
+        .split_whitespace()
+        .map(|v| v.parse())
+        .collect::<Result<_, _>>()?;
+
+    let depth_increases = get_depth_increases(&values);
 
     println!("Total Depth Increases: {}", depth_increases);
 
@@ -33,8 +31,8 @@ mod test {
 
     #[test]
     fn example() {
-        let input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        assert_eq!(get_depth_increases(&mut input.into_iter()), 7);
+        assert_eq!(get_depth_increases(&input), 7);
     }
 }
