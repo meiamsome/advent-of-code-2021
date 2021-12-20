409fd2d754fa53c82ed2cbbf8a9f2644ddb26548
Oliver Wright <meiamsome@meiamso.me>
2021-12-20 23:24:45 +0000

    D4P1: Add get win time for board

--- day04/part1/src/main.rs
+++ day04/part1/src/main.rs
@@ -75,6 +75,19 @@ fn get_winning_combinations<'a>(board: &'a Board) -> impl Iterator<Item = [u8; 5
     })
 }
 
+fn get_win_time(numbers_called: &HashMap<u8, usize>, board: &Board) -> usize {
+    get_winning_combinations(board)
+        .map(|combo| {
+            combo
+                .into_iter()
+                .map(|n| *numbers_called.get(&n).ok_or(usize::MAX).unwrap())
+                .max()
+                .unwrap()
+        })
+        .min()
+        .unwrap()
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut contents = String::new();
     {
@@ -226,5 +239,51 @@ mod test {
                 [0, 24, 7, 5, 19],
             ],
         );
+    }
+
+    #[test]
+    fn example_board_1_win_time() {
+        let numbers_called = called_numbers_to_map(
+            &mut vec![
+                7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8,
+                19, 3, 26, 1,
+            ]
+            .into_iter(),
+        );
+
+        let board = [
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19],
+        ];
+
+        let time = get_win_time(&numbers_called, &board);
+
+        assert_eq!(time, 13);
+    }
+
+    #[test]
+    fn example_board_3_win_time() {
+        let numbers_called = called_numbers_to_map(
+            &mut vec![
+                7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8,
+                19, 3, 26, 1,
+            ]
+            .into_iter(),
+        );
+
+        let board = [
+            [14, 21, 17, 24, 4],
+            [10, 16, 15, 9, 19],
+            [18, 8, 23, 26, 20],
+            [22, 11, 13, 6, 5],
+            [2, 0, 12, 3, 7],
+        ];
+
+        let time = get_win_time(&numbers_called, &board);
+
+        assert_eq!(time, 11);
     }
 }
