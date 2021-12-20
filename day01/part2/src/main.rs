72c99e1eef4731affb22bddaea5fe29974141da3
Oliver Wright <meiamsome@meiamso.me>
2021-12-20 19:32:32 +0000

    Day 1 Part 2

---
+++ day01/part2/src/main.rs
@@ -0,0 +1,38 @@
+use std::{fs::File, io::Read};
+
+fn get_depth_increases(list: &Vec<u16>) -> usize {
+    list.windows(4)
+        .map(|res| if res[0] < res[3] { 1 } else { 0 })
+        .sum()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut contents = String::new();
+    {
+        let mut file = File::open("./day01/part1/input.txt")?;
+        file.read_to_string(&mut contents)?;
+    }
+
+    let values = contents
+        .split_whitespace()
+        .map(|v| v.parse())
+        .collect::<Result<_, _>>()?;
+
+    let depth_increases = get_depth_increases(&values);
+
+    println!("Total Depth Increases: {}", depth_increases);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(get_depth_increases(&input), 5);
+    }
+}
